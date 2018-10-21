a166736f14fcacc964733b43a75895fe3abd55ac
Peter Bägel (DF5EQ) <peter@baegel.de>
2018-10-21 22:35:13 +0200

    some typos corrected

--- src/scan.c
+++ src/scan.c
@@ -114,8 +114,8 @@ static unsigned int modulation_min = 0;	// initialization of modulation loop. QA
 static unsigned int modulation_max = 1;	// initialization of modulation loop. QAM256 if FE_QAM
 static unsigned int dvbc_symbolrate_min = 0;	// initialization of symbolrate loop. 6900
 static unsigned int dvbc_symbolrate_max = 1;	// initialization of symbolrate loop. 6875
-static unsigned int plp_id_min = 0; // initialization of delsys loop.
-static unsigned int plp_id_max = 0; // initialization of delsys loop.
+static unsigned int plp_id_min = 0; // initialization of plp_id loop.
+static unsigned int plp_id_max = 0; // initialization of plp_id loop.
 static unsigned int freq_offset_min = 0;	// initialization of freq offset loop. 0 == offset (0), 1 == offset(+), 2 == offset(-), 3 == offset1(+), 4 == offset2(+)
 static unsigned int freq_offset_max = 4;	// initialization of freq offset loop.
 static int this_channellist = DVBT_DE;	// w_scan2 uses by default DVB-t
